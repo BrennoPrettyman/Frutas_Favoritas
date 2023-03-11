c8a34386f4e38bc5fc717dadfcdb3a54df74d4d6
Brenno Prettyman <brennoprettyman@gmail.com>
2023-03-11 19:18:18 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Frutas_Favoritas;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("---Minhas Frutas Favoritas---");
+        Console.ResetColor();
+
+        Console.WriteLine();
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("1. Maçã");
+        Console.WriteLine("2. Banana");
+        Console.WriteLine("3. Melancia");
+        Console.WriteLine("4. Uva");
+        Console.WriteLine("5. Manga");
+        Console.ResetColor();
+    }
+}
